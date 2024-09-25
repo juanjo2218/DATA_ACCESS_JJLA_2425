73c91c2c46b97e79803aba97c2c9944ec347e6f9
juanjo2218 <juanjo22182218@gmail.com>
2024-09-25 20:47:13 +0200

    uh8

--- Unit2/ex4.4.3/src/Main.java
+++ Unit2/ex4.4.3/src/Main.java
@@ -1,5 +1,4 @@
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -14,31 +13,56 @@ public class Main {
 }
 class Ex
 {
+
     public static void  adas() throws IOException {
         Scanner myScanner = new Scanner(System.in);
         String path = myScanner.nextLine();
-        FileInputStream file = new FileInputStream(path);
-        byte[] data = new byte[26];
-        if (file.read(data) != -1)
-        {
-            if(IsBMP(data))
+        try {
+            FileInputStream file = new FileInputStream(path);
+            byte[] data = new byte[30];
+            if (file.read(data) != -1)
             {
-                System.out.println("adfsad");
-                long dad = readSize(data,22,4);
-            }
+                if(IsBMP(data))
+                {
 
+                    int INITBYTESIZE = 2;
+                    int BYTEQUANITYSIZE = 4;
+                    int INITBYTEWIDTH = 18;
+                    int BYTEQUANITYWIDTH= 4;
+                    int INITBYTEHEIGHT = 22;
+                    int BYTEQUANITYHEIGHT = 4;
+                    int INITBYTEBPP = 28;
+                    int BYTEQUANITYBPP= 2;
+                    System.out.println("Size: " + toInt(data,INITBYTESIZE,BYTEQUANITYSIZE) + " bytes");
+                    System.out.println("Width : " + toInt(data,INITBYTEWIDTH,BYTEQUANITYWIDTH) + " pixels");
+                    System.out.println("Height : " + toInt(data,INITBYTEHEIGHT,BYTEQUANITYHEIGHT) + " pixels");
+                    System.out.println("Bits for píxel: " + toInt(data,INITBYTEBPP,BYTEQUANITYBPP) + " bits");
+                }
+                else
+                    System.out.println("NOT BMP");
+            }
+            else
+            {
+                System.out.println("FILE_EMPTY");
+            }
         }
+        catch (Exception ignored)
+        {
+            System.out.println("PATH_INCORRECT");
+        }
+
     }
     public static boolean IsBMP(byte[] file)
     {
         return file[0] == 0x42 && file[1] == 0x4d ;
     }
-    public static long readSize(byte[] file,int offset,int quantity)
+
+    public static long toInt(byte[] file,int offset,int quantity)
     {
         long total = 0;
         for (int i = 0 ; i < quantity; i++)
         {
-
+            total += (long) ((file[offset + i] & 0xFF) * Math.pow(256, i));
         }
         return total;
     }
