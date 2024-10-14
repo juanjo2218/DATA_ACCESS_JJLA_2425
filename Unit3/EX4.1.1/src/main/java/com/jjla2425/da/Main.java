17feb69be9ad0966c4dacbf18b75fce4310981f9
juanjo2218 <juanjo22182218@gmail.com>
2024-10-14 20:40:27 +0200

    hbu

---
+++ Unit3/EX4.1.1/src/main/java/com/jjla2425/da/Main.java
@@ -0,0 +1,34 @@
+package com.jjla2425.da;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args)
+    {
+        String url = "jdbc:postgresql://localhost:5432/VTInstitute";
+        String user = "postgres";
+        String password = "postgres";
+        try(Connection con = DriverManager.getConnection(url, user, password);)
+        {
+            Statement statement = con.createStatement();
+            String SQLsentence = "SELECT * FROM subjects ORDER BY code";
+            ResultSet rs = statement.executeQuery(SQLsentence);
+            System.out.println("Code" + "\t" + "Name");
+            System.out.println("-----------------------------------------");
+            while (rs.next())
+            {
+                System.out.println(rs.getString("name"));
+            }
+            rs.close();
+        }
+        catch (Exception e)
+        {
+        }
+
+    }
+}
