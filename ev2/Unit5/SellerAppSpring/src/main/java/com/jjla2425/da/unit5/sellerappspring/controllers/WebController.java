1c82d3fa5917c1f206c3ca121fae0fee50e25d6a
juanjo2218 <juanjo22182218@gmail.com>
2025-01-15 20:40:36 +0100

    ssss

--- ev2/Unit5/SellerAppSpring/src/main/java/com/jjla2425/da/unit5/sellerappspring/controllers/WebController.java
+++ ev2/Unit5/SellerAppSpring/src/main/java/com/jjla2425/da/unit5/sellerappspring/controllers/WebController.java
@@ -1,12 +1,19 @@
 package com.jjla2425.da.unit5.sellerappspring.controllers;
 
+import com.jjla2425.da.unit5.sellerappspring.model.entities.CategoriesEntity;
+import com.jjla2425.da.unit5.sellerappspring.model.entities.ProductsEntity;
 import com.jjla2425.da.unit5.sellerappspring.model.entities.SellersEntity;
+import com.jjla2425.da.unit5.sellerappspring.services.CategoriesService;
+import com.jjla2425.da.unit5.sellerappspring.services.ProductsService;
+import com.jjla2425.da.unit5.sellerappspring.services.SellerProductService;
 import com.jjla2425.da.unit5.sellerappspring.services.SellerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -14,37 +21,54 @@ import java.util.List;
 public class WebController {
     @Autowired
     private SellerService sellerService;
+    @Autowired
+    private SellerProductService sellerProductService;
+    @Autowired
+    private CategoriesService categoriesService;
 
-
+    @Autowired
+    private ProductsService productsService;
     @GetMapping("/")
     public String showLogin(Model model)
     {
         return "login";
     }
-    @GetMapping("/viewseller/{cif}")
-    public String viewSeller(@PathVariable(value = "cif") String CIF, Model model)
-    {
+    @GetMapping("/viewseller")
+    public String viewSeller(@RequestParam(value = "cif", required = true) String CIF, Model model) {
+        // Buscar el seller por el CIF
         SellersEntity seller = sellerService.findSellerBycif(CIF).getBody();
 
-        // Si el seller no se encuentra, puedes redirigir a una página de error o mostrar un mensaje
+        // Si no se encuentra el seller, redirigir a una página de error o mostrar un mensaje
         if (seller == null) {
             model.addAttribute("error", "Seller not found");
-            return "error";  // Redirigir a una página de error
+            return "error"; // Redirigir a una página de error
         }
 
-        model.addAttribute("seller", seller);  // Pasar el objeto seller a la plantilla
-        return "viewseller";  // Nombre de
+        // Pasar el objeto seller al modelo para usarlo en la plantilla
+        model.addAttribute("seller", seller);
+        return "viewseller"; // Nombre de la plantilla Thymeleaf
     }
+
     @GetMapping("/addproduct")
-    public String addProduct(String CIF,Model model)
-    {
-        model.addAttribute("seller",sellerService.findSellerBycif(CIF));
+    public String addProduct(@RequestParam(value = "categoryId", required = false) Integer categoryId, Model model) {
+        List<CategoriesEntity> categories = categoriesService.findAllCategories();
+        model.addAttribute("categories", categories);
+
+        if (categoryId != null && categoryId != 0) {
+            List<ProductsEntity> products = productsService.findAllProductsByCategoryAndActive(categoryId);
+            model.addAttribute("products", products);
+            model.addAttribute("selectedCategoryId", categoryId);  // Pasa el ID de la categoría seleccionada
+        }
+
         return "addproduct";
     }
+
     @GetMapping("/addoffer")
-    public String addOffer(String CIF,Model model)
+    public String addOffer(@RequestParam(value = "cif", required = true) String CIF,Model model)
     {
-        model.addAttribute("seller",sellerService.findSellerBycif(CIF));
+        SellersEntity sellersEntity = sellerService.findSellerBycif(CIF).getBody();
+        model.addAttribute("sellerproducts", sellerProductService.findAllSellerProductsByIdSellerAndActive(sellersEntity.getSellerId()));
+        model.addAttribute("seller",sellerService.findSellerBycif(sellersEntity.getCif()));
         return "addoffer";
     }
 }
